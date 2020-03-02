2e9526dffba3eaaa5f88740e846dc2c1aae071bf
vince <vince@air.local>
2020-03-01 16:35:06 -0800

    cleanup

--- vincetest.py
+++ vincetest.py
@@ -40,11 +40,10 @@ Any hacks/errors/omissions/crimes-against-python are all mine.
       echo 'select * from archive; | sqlite3 /home/weewx/archive/weewx.sdb
     
 '''
+
 import json
 import requests
 import sys
-
-import serial   # vds: not needed in the real json/http based driver, to be deleted
 import syslog
 import time
 
@@ -53,7 +52,7 @@ import weewx.engine
 import weewx.units
 
 DRIVER_NAME = "vincetest"
-DRIVER_VERSION = "0.0.1"
+DRIVER_VERSION = "0.0.2"
 
 def logmsg(dst, msg):
     syslog.syslog(dst, 'vincetest: %s' % msg)
@@ -67,23 +66,14 @@ def loginf(msg):
 def logerr(msg):
     logmsg(syslog.LOG_ERR, msg)
 
+# do we need this ?
 def loader(config_dict, engine):
     return vincetestDriver(**config_dict['vincetest'])
 
-schema = [('dateTime',  'INTEGER NOT NULL UNIQUE PRIMARY KEY'),
-          ('usUnits',   'INTEGER NOT NULL'),
-          ('interval',  'INTEGER NOT NULL'),
-          ('range',     'REAL')]
-
-weewx.units.obs_group_dict['range'] = 'group_range'
-weewx.units.obs_group_dict['range2'] = 'group_range'
-weewx.units.obs_group_dict['range3'] = 'group_range'
-weewx.units.USUnits['group_range'] = 'inch'
-weewx.units.MetricUnits['group_range'] = 'cm'
-weewx.units.MetricWXUnits['group_range'] = 'cm'
-
-
 class vincetestDriver(weewx.drivers.AbstractDevice):
+
+    # the quick poll_interval lets us run --test-driver and see info quicker
+    # in general this shouldn't be faster than 60 secs, defined in weewx.conf
 
     def __init__(self, **stn_dict):
         loginf("driver version is %s" % DRIVER_VERSION)
@@ -91,8 +81,9 @@ class vincetestDriver(weewx.drivers.AbstractDevice):
         self.retry_wait = int(stn_dict.get('retry_wait', 10))
         self.poll_interval = float(stn_dict.get('poll_interval', 2))
         loginf("polling interval is %s" % self.poll_interval)
-        self.url = stn_dict.get('url', 'http://192.168.1.18:80/conditions.json')
+        self.url = stn_dict.get('url', 'http://192.168.1.18:80/v1/conditions')
 
+    # delete me
     @property
     def hardware_name(self):
         return "vincetest"
@@ -103,18 +94,36 @@ class vincetestDriver(weewx.drivers.AbstractDevice):
         while ntries < self.max_tries:
             ntries += 1
             try:
-                r = requests.get(url=self.url)
-                data = r.json()
-                # print('\n') ; print(data); print('\n')    # vdsdebug
 
-                # could have multiple sensors with varying content
-                # so we need to loop through the sensors individually
+                # "expecting value: line 1 column 1 (char 0)" is returned if not JSON
+                # or if the file being requested isn't found.  Ideally we should key off
+                # the return status from the webserver, but servers returning a default
+                # web page will return 200 when they should actually 404, meaning that if
+                # anything goes wrong here, syslog isn't going to tell you which failed here
+
+                try:
+                    r = requests.get(url=self.url)
+                    data = r.json()
+                    # print('\n') ; print(data); print('\n')    # vdsdebug
+                except Exception as e:
+                    loginf("failure to get data - try %s - (%s)" % (ntries, e))
+                    if self.poll_interval:
+                        time.sleep(self.poll_interval)
+                    return
+
+                # if you got to here, you should have valid JSON to parse
+
+                # to do - punt back to the loop unless data['data']['errors'] == 'none'
+
+                #-----------------------------------------------------------------------------
+                # The WeatherLink Live returns JSON containing an array of sensors with their 
+                # individual unique content, so we need to loop through the sensors one by one
                 #
-                # also each sensor has different content
                 # see https://weatherlink.github.io/weatherlink-live-local-api/
-
+                #
                 # the mappings below are incomplete but should be enough
                 # to test that weewx will generate at least some graphs
+                #-----------------------------------------------------------------------------
 
                 for s in data['data']['conditions']:
 
@@ -138,6 +147,9 @@ class vincetestDriver(weewx.drivers.AbstractDevice):
                         inTemp = s['temp_in']
                         inHumidity = s['hum_in']
 
+                # got the data ok so reset the flag counter
+                ntries = 0
+
                 _packet = {'dateTime': int(time.time() + 0.5),
                            'usUnits': weewx.US,
                            'outTemp': outTemp,
@@ -147,20 +159,11 @@ class vincetestDriver(weewx.drivers.AbstractDevice):
                            'windSpeed' : windSpeed,
                            'windDir' : windDir,
                            }
-
-                #vds: commented out mwalls' example - we'd do our json query and packet construction here
-                #with Sensor(self.model, self.port) as sensor:
-                    #v = sensor.get_range()
-                ntries = 0
-                # vds - this is a clearly bogus packet for test purposes
-                # _packet = {'dateTime': int(time.time() + 0.5),
-                           # 'usUnits': weewx.US,
-                           # 'outTemp': 31.23 }
                 yield _packet
                 if self.poll_interval:
                     time.sleep(self.poll_interval)
-            # vds: this won't check for serial connection in a real json http query driver of course
-            except (serial.serialutil.SerialException, weewx.WeeWxIOError) as e:
+
+            except (weewx.WeeWxIOError) as e:
                 logerr("Failed attempt %d of %d to get LOOP data: %s" %
                        (ntries, self.max_tries, e))
                 time.sleep(self.retry_wait)
@@ -171,75 +174,9 @@ class vincetestDriver(weewx.drivers.AbstractDevice):
 
 
 class Sensor():
-
-    # information about each type of sensor.  the key is the model number.  the
-    # associated tuple contains the units of the value that is returned, the
-    # value the sensor returns when the range is maxxed out, and the number or
-    # characters (excluding the R and trailing newline) in the value string.
-    MODEL_INFO = {
-        'MB1040': ['inch', 254, 3], # 6in min; 254in max; 1in res
-        }
-
-    def __init__(self, model, port, baudrate=9600, timeout=1):
+    def __init__(self, model):
         self.model = model
-        self.port = port
-        self.baudrate = baudrate
         self.timeout = timeout
-        self.serial_port = None
-        model_info = Sensor.MODEL_INFO[self.model]
-        self.units = model_info[0]
-        self.no_target = model_info[1]
-        self.data_length = model_info[2]
-        self.JSON=None
-
-    def __enter__(self):
-        self.open()
-        return self
-
-    def __exit__(self, _, value, traceback):
-        self.close()
-
-    def open(self):
-        self.serial_port = serial.Serial(self.port, self.baudrate,
-                                         timeout=self.timeout)
-
-    def close(self):
-        if self.serial_port is not None:
-            self.serial_port.close()
-            self.serial_port = None
-
-# this is where we'd get the json and deconstruct it
-    def get_range(self):
-        # return value is always mm
-        line = self.serial_port.read(self.data_length + 2)
-        if line:
-            line = line.strip()
-        if line and len(line) == self.data_length + 1 and line[0] == 'R':
-            try:
-                v = int(line[1:])
-                if v == self.no_target:
-                    logdbg("no target detected: v=%s" % v)
-                    v = None
-                if self.units == 'inch':
-                    v *= 25.4
-                return v
-            except ValueError as e:
-                raise weewx.WeeWxIOError("bogus value: %s" % e)
-        else:
-            raise weewx.WeeWxIOError("unexpected line: '%s'" % line)
-
-    def get_json(self):
-        print(self.JSON)
-
-    def getJSON(self):
-        # get the data
-        r = requests.et(url="http://192.168.1.18:80/conditions.json")
-        data = r.json()
-        if data['error']:
-            print("error in json returned")
-        else:
-            print(data['data']['ts'])
-        return data
 
 # To test this driver, do the following:
 #   PYTHONPATH=/home/weewx/bin python /home/weewx/bin/user/vincetest.py
@@ -250,9 +187,6 @@ if __name__ == "__main__":
         import optparse
         syslog.openlog('wee_vincetest', syslog.LOG_PID | syslog.LOG_CONS)
         parser = optparse.OptionParser(usage=usage)
-        parser.add_option('--url', dest="url", metavar="URL ",
-                default='http://192.168.1.18:80/conditions.json',
-                help="The URL to query.  Default is 'http://192.168.1.18:80/conditions.json'")
         parser.add_option('--test-driver', dest='td', action='store_true',
                           help='test the driver')
         (options, args) = parser.parse_args()
